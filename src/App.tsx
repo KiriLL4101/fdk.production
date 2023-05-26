06438434152664c7a1a7a9ffaa1f66e4a42bf75e
KiriLL4101 <fedechkin-kiril@mail.ru>
2023-05-26 15:14:00 +0300

    style: dark and normal style variable also add toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
-import { Counter } from './components/Counter'
-import './index.scss'
-import { MainLazy } from './pages/Main.lazy'
-import { AboutLazy } from './pages/About.lazy'
 import { Link } from 'react-router-dom'
 import { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { MainLazy } from './pages/Main.lazy'
+import { AboutLazy } from './pages/About.lazy'
+
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
 
 export const App = () => {
+  const { toggleTheme } = useTheme()
+
   return (
     <div className="app">
+      <button onClick={toggleTheme}>Theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>about</Link>
 
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,42 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import { useContext } from 'react'
+
+interface UseThemeOutput {
+  theme: Theme
+  toggleTheme: () => void
+}
+
+export function useTheme(): UseThemeOutput {
+  const context = useContext(ThemeContext)
+
+  //  if (!context) {
+  //     throw new Error('useTheme hook must be used within a Theme Provider')
+  //   }
+
+  const { theme, setTheme } = context
+
+  const toggleTheme = () => {
+    let newTheme: Theme
+
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT
+        break
+      case Theme.LIGHT:
+        newTheme = Theme.DARK
+        break
+      default:
+        newTheme = Theme.LIGHT
+    }
+
+    setTheme?.(newTheme)
+
+    document.body.className = newTheme
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  }
+
+  return {
+    theme: theme || Theme.LIGHT,
+    toggleTheme
+  }
+}
