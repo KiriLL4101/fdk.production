cf56a97f96d53984bd98583b42925252b64810d6
KiriLL4101 <fedechkin-kiril@mail.ru>
2023-07-18 21:02:12 +0300

    feat: blue theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,44 +1,48 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+/* eslint-disable indent */
+import { useContext } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
 
 interface UseThemeOutput {
-  theme: Theme
-  toggleTheme: () => void
+    theme: Theme
+    toggleTheme: () => void
 }
 
 export function useTheme(): UseThemeOutput {
-    const context = useContext(ThemeContext);
+    const context = useContext(ThemeContext)
 
     //  if (!context) {
     //     throw new Error('useTheme hook must be used within a Theme Provider')
     //   }
 
-    const { theme, setTheme } = context;
+    const { theme, setTheme } = context
 
     const toggleTheme = () => {
-        let newTheme: Theme;
+        let newTheme: Theme
 
         switch (theme) {
-        case Theme.DARK:
-            newTheme = Theme.LIGHT;
-            break;
-        case Theme.LIGHT:
-            newTheme = Theme.DARK;
-            break;
-        default:
-            newTheme = Theme.LIGHT;
+            case Theme.DARK:
+                newTheme = Theme.LIGHT
+                break
+            case Theme.LIGHT:
+                newTheme = Theme.BLUE
+                break
+            case Theme.BLUE:
+                newTheme = Theme.DARK
+                break
+            default:
+                newTheme = Theme.LIGHT
         }
 
-        setTheme?.(newTheme);
+        setTheme?.(newTheme)
 
-        document.body.classList.value = '';
-        document.body.classList.add(newTheme);
+        document.body.classList.value = ''
+        document.body.classList.add(newTheme)
 
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    };
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    }
 
     return {
         theme: theme || Theme.LIGHT,
         toggleTheme,
-    };
+    }
 }
