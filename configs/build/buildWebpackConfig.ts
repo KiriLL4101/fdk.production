10edc6ffb0690c74e402d5839b14388db3ee30dc
KiriLL4101 <fedechkin-kiril@mail.ru>
2023-07-19 09:54:57 +0300

    feat: concurrently package

--- configs/build/buildWebpackConfig.ts
+++ configs/build/buildWebpackConfig.ts
@@ -1,13 +1,11 @@
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import { buildPlugins } from './buildPlugins'
-import { buildRules } from './buildRules'
+import { buildRules } from './buildLoaders'
 import { buildResolvers } from './buildResolvers'
 import { buildDevServer } from './buildDevServer'
 
-export function buildWebpackConfig(
-    options: BuildOptions
-): webpack.Configuration {
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const { mode, paths, isDev } = options
 
     return {
@@ -16,14 +14,14 @@ export function buildWebpackConfig(
         output: {
             filename: '[name].[contenthash].js',
             path: paths.build,
-            clean: true
+            clean: true,
         },
         plugins: buildPlugins(options),
         module: {
-            rules: buildRules(options)
+            rules: buildRules(options),
         },
         resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
